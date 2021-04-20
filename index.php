274adae5634f7fd80a5ca5a0bd934a525191decb
bapt <baptiste.gauthier@laplateforme.io>
2021-04-20 18:06:56 +0200

    style ok

--- index.php
+++ index.php
@@ -5,13 +5,32 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link rel="stylesheet" href="style/style.css">
     </head>
     <body>
 
-        <button id="get_json"> get json </button>
+        <header>
+            <div>
+                <img src="images/tri_force.svg" alt="triforce">
+            </div>    
 
-        <div id="contenu"></div>
-        
+        </header>
+
+        <main>
+            <section id="search">
+                <article class="content_search">
+                    <div class="title">
+                        <h1> Breath of the Wild - <span>Monster search</span> </h1>
+                    </div>
+                    <div class="bar">
+                            <input type="search" name="bar" id="search_bar">
+                            <button id="button_search"> Search </button>    
+                    </div>
+                </article>    
+            </section>
+
+        </main>
+  
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="script.js"></script>
     </body>
--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 
 // permet d'ajouter en bdd toutes les infos du json 
 
-$(document).on("click" , "#get_json" , () => {
+//-----------------------$(document).on("click" , "#get_json" , () => {
+//-----------------------
+//-----------------------    
+//-----------------------    $.ajax({
+//-----------------------        type: "GET",
+//-----------------------        url: "botw.json",
+//-----------------------        dataType: "json",
+//-----------------------        success: function (data) {
+//-----------------------            console.log(data) ; 
+//-----------------------            for(var i = 0 ; i <= 80 ; i++)
+//-----------------------            {
+//-----------------------                
+//-----------------------                var name = data.data.monsters[i].name ; 
+//-----------------------                var description = data.data.monsters[i].description ; 
+//-----------------------                var image = data.data.monsters[i].image;
+//-----------------------                if(data.data.monsters[i].common_locations == null)
+//-----------------------                {
+//-----------------------                    var common_locations = "" ; 
+//-----------------------                }
+//-----------------------                else{
+//-----------------------                    var common_locations = data.data.monsters[i].common_locations[0];
+//-----------------------                }
+//-----------------------
+//-----------------------                $.ajax({
+//-----------------------                    type: "POST",
+//-----------------------                    url: "add_infos_bdd.php",
+//-----------------------                    data: {name: name, description: description, image: image, common_locations: common_locations},
+//-----------------------                    dataType: "text",
+//-----------------------                    success: function (response) {
+//-----------------------                        $('body').append(response); 
+//-----------------------                    }
+//-----------------------                });
+//-----------------------
+//-----------------------                
+//-----------------------            }
+//-----------------------        }
+//-----------------------    });
+//-----------------------})
 
-    
-    $.ajax({
-        type: "GET",
-        url: "botw.json",
-        dataType: "json",
-        success: function (data) {
-            console.log(data) ; 
-            for(var i = 0 ; i <= 80 ; i++)
-            {
-                
-                var name = data.data.monsters[i].name ; 
-                var description = data.data.monsters[i].description ; 
-                var image = data.data.monsters[i].image;
-                if(data.data.monsters[i].common_locations == null)
-                {
-                    var common_locations = "" ; 
-                }
-                else{
-                    var common_locations = data.data.monsters[i].common_locations[0];
-                }
-
-                $.ajax({
-                    type: "POST",
-                    url: "add_infos_bdd.php",
-                    data: {name: name, description: description, image: image, common_locations: common_locations},
-                    dataType: "text",
-                    success: function (response) {
-                        $('body').append(response); 
-                    }
-                });
-
-                
-            }
-        }
-    });
-})
